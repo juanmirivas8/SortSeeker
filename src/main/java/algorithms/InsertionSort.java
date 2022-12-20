1d6a7f950a41c1d2efd88f628d9d6986b468d36d
juanmirivas8 <juanmirivas8@gmail.com>
2022-12-21 00:10:05 +0100

    Subida de los algoritmos de ordenacion

---
+++ src/main/java/algorithms/InsertionSort.java
@@ -0,0 +1,30 @@
+package algorithms;
+
+/**
+ * InsertionSort.java
+ * Created by Stijn Strickx on May 21, 2008
+ * Copyright 2008 Stijn Strickx, All rights reserved
+ */
+
+/**
+ * Insertion sort algorithm
+ * Time Complexity: O(n*n)
+ * Memory Complexity: O(1)
+ * Stable: yes
+ */
+
+public class InsertionSort extends Sorter{
+
+    @Override
+    public <T extends Comparable<? super T>> void sort(T[] a) {
+        for(int i = 1; i < a.length; i++){
+            int j = i;
+            T o = a[i];
+            while((j > 0) && o.compareTo(a[j-1]) < 0){
+                a[j] = a[j-1];
+                j--;
+            }
+            a[j] = o;
+        }
+    }
+}
